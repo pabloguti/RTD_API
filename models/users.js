51e86a642cb88c112148f8b8095977fc6894db3f
Pablo Gutierrez <pablo.gutierrezdiez@cognizant.com>
2017-03-13 20:39:05 +0100

    New model

--- models/users.js
+++ models/users.js
@@ -2,7 +2,8 @@ exports = module.exports = function(app, mongoose) {
 
     var userSchema = new mongoose.Schema({
         name: {type: String},
-        email: {type: String}
+        email: {type: String},
+        technical: {type: String}
     }, {collection: 'users'});
 
     mongoose.model('Users', userSchema);
